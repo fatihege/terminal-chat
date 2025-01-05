e1df5563118cf7cac8801e46461a03ef466eeab9
fatihege <ifatxh@gmail.com>
2025-01-05 15:01:52 +0300

    implement new commands like ban, kick to the server code and refactor the scripts

--- client.cpp
+++ client.cpp
@@ -7,51 +7,75 @@
 
 #define COLUMN_WIDTH 100
 
+using std::string;
+using std::cout;
+using std::cin;
+using std::getline;
+using std::thread;
+
 std::mutex outputMutex;
-std::string input;
-std::string username;
-std::string ip = "127.0.0.1";
+string input;
+string username;
+string ip = "127.0.0.1";
 unsigned short port = 8080;
-const std::string separator(COLUMN_WIDTH, '-');
+const string separator(COLUMN_WIDTH, '-');
+const string banMessage = "banned";
+const string kickMessage = "kicked";
 
 void removeLastLine();
 
-void writeMessage(const std::string &message);
+void writeMessage(const string &message);
 
 int main() {
-    std::cout << "Enter username: ";
-    std::getline(std::cin, input);
+    cout << "Enter username: ";
+    getline(cin, input);
     if (!input.empty()) {
         username = input;
     }
 
-    std::cout << "Enter server IP (default " << ip << "): ";
-    std::getline(std::cin, input);
+    for (const char c: username) {
+        if (!isalnum(c)) {
+            cout << "Invalid username. Use alphanumeric characters only.\n";
+            cin.get();
+            return 1;
+        }
+    }
+
+    if (username.empty() || username.size() > 100) {
+        cout << "Invalid username. Please restart and use a valid username (1-100 characters).\n";
+        cin.get();
+        return 1;
+    }
+
+    cout << "Enter server IP (default " << ip << "): ";
+    getline(cin, input);
     if (!input.empty()) {
         ip = input;
     }
 
-    std::cout << "Enter port (default " << port << "): ";
-    std::getline(std::cin, input);
+    cout << "Enter port (default " << port << "): ";
+    getline(cin, input);
     if (!input.empty()) {
         try {
             if (const unsigned short newPort = std::stoi(input); newPort > 0 && newPort <= 65535) port = newPort;
-            else std::cout << "Invalid port range. Using default.\n";
+            else cout << "Invalid port range. Using default.\n";
         } catch (...) {
-            std::cout << "Invalid input. Using default port.\n";
+            cout << "Invalid input. Using default port.\n";
         }
     }
 
     WSADATA wsaData;
     if (const int result = WSAStartup(MAKEWORD(2, 2), &wsaData); result != 0) {
-        std::cout << "WSAStartup failed. Error: " << result << '\n';
+        cout << "WSAStartup failed. Error: " << result << '\n';
+        cin.get();
         return 1;
     }
 
     const auto clientSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (clientSocket == INVALID_SOCKET) {
-        std::cout << "Socket creation failed. Error: " << WSAGetLastError() << '\n';
+        cout << "Socket creation failed. Error: " << WSAGetLastError() << '\n';
         WSACleanup();
+        cin.get();
         return 1;
     }
 
@@ -60,51 +84,47 @@ int main() {
     serverAddress.sin_port = htons(port);
 
     if (inet_pton(AF_INET, ip.c_str(), &serverAddress.sin_addr) != 1) {
-        std::cout << "Invalid IP address format.\n";
+        cout << "Invalid IP address format.\n";
         closesocket(clientSocket);
         WSACleanup();
+        cin.get();
         return 1;
     }
 
     if (connect(clientSocket, reinterpret_cast<sockaddr *>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
-        std::cout << "Connection failed. Error: " << WSAGetLastError() << '\n';
+        cout << "Connection failed. Error: " << WSAGetLastError() << '\n';
         closesocket(clientSocket);
         WSACleanup();
-        return 1;
-    }
-
-    if (username.empty() || username.size() > 100) {
-        std::cout << "Invalid username. Please restart and use a valid username (1-100 characters).\n";
-        closesocket(clientSocket);
-        WSACleanup();
+        cin.get();
         return 1;
     }
 
     if (send(clientSocket, username.c_str(), username.size(), 0) == SOCKET_ERROR) {
-        std::cout << "Couldn't send the username. Error: " << WSAGetLastError() << '\n';
+        cout << "Couldn't send the username. Error: " << WSAGetLastError() << '\n';
         closesocket(clientSocket);
         WSACleanup();
+        cin.get();
         return 1;
     }
 
-    std::cout << "Connected. Type '/help' to see the commands, '/exit' to disconnect.\n\n\n";
+    cout << "Connected. Type '/help' to see the commands, '/exit' to disconnect.\n\n\n";
 
-    std::thread sendThread([&] {
-        std::string message;
-        while (std::getline(std::cin, message)) {
+    thread sendThread([&] {
+        string message;
+        while (getline(cin, message)) {
             if (message == "/exit") {
                 shutdown(clientSocket, SD_SEND);
                 break;
             }
 
             if (message == "/help") {
-                std::cout << "\nThere'll be something here.\n\n\n";
+                cout << "\nThere'll be something here.\n\n\n";
                 continue;
             }
 
             if (send(clientSocket, message.c_str(), static_cast<int>(message.size()), 0) == SOCKET_ERROR) {
                 std::lock_guard lock(outputMutex);
-                std::cout << "Send failed. Error: " << WSAGetLastError() << '\n';
+                cout << "Send failed. Error: " << WSAGetLastError() << '\n';
                 break;
             }
 
@@ -113,17 +133,32 @@ int main() {
         }
     });
 
-    std::thread receiveThread([&] {
+    thread receiveThread([&] {
         while (true) {
-            char buffer[1024 + 1];
-            const int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+            constexpr int BUFFER_SIZE = 1024;
+            char buffer[BUFFER_SIZE + 1];
+            const int bytesReceived = recv(clientSocket, buffer, BUFFER_SIZE, 0);
 
             if (bytesReceived <= 0) {
-                std::cout << "Disconnected from server.\n";
+                cout << "Disconnected from server.\n";
+                closesocket(clientSocket);
+                WSACleanup();
                 break;
             }
 
-            buffer[bytesReceived] = '\0'; {
+            buffer[bytesReceived] = '\0';
+
+            if (const string message(buffer); message == banMessage) {
+                cout << "You're banned from this server.\n";
+                closesocket(clientSocket);
+                WSACleanup();
+                break;
+            } else if (message == kickMessage) {
+                cout << "You're kicked from this server.\n";
+                closesocket(clientSocket);
+                WSACleanup();
+                break;
+            } {
                 std::lock_guard lock(outputMutex);
                 writeMessage(buffer);
             }
@@ -139,18 +174,18 @@ int main() {
 }
 
 void removeLastLine() {
-    std::cout << "\033[A\033[K";
+    cout << "\033[A\033[K";
 }
 
-void writeMessage(const std::string &message) {
+void writeMessage(const string &message) {
     removeLastLine();
-    std::cout << separator << '\n';
+    cout << separator << '\n';
 
     int start = 0;
     while (start < message.size()) {
         int end = start + COLUMN_WIDTH;
         if (end >= message.size()) end = message.size();
-        std::cout << message.substr(start, end - start) << "\n\n";
+        cout << message.substr(start, end - start) << "\n\n";
         start = end;
     }
 }
--- main.cpp
+++ main.cpp
@@ -5,165 +5,354 @@
 #include <vector>
 #include <mutex>
 #include <atomic>
+#include <fstream>
 #include <string>
+
+#define BANS_FILE "bans"
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::getline;
+using std::vector;
+using std::thread;
 
 struct Client {
     SOCKET socket;
-    std::string username;
+    string username;
+    string ip;
 };
 
-std::string input;
-std::vector<Client> clients;
+string input;
+vector<Client> clients;
+vector<string> bans;
 std::mutex clientsMutex;
 std::atomic isRunning(true);
 unsigned short port = 8080;
+const string banMessage = "banned";
+const string kickMessage = "kicked";
+
+void initialConfiguration();
+
+void acceptClients(const auto &serverSocket);
+
+int bindAndListen(const auto &serverSocket);
+
+void loadBans();
+
+void banClient(const SOCKET &socket);
+
+string clientInfo(const Client &client);
+
+string getClientIp(const SOCKET &clientSocket);
 
 void removeClient(SOCKET clientSocket);
 
 void handleClient(SOCKET clientSocket);
 
-void broadcastMessage(const std::string &message, SOCKET senderSocket);
+void broadcastMessage(const string &message, SOCKET senderSocket);
 
-void listenExit(SOCKET serverSocket);
+void listenCommands(SOCKET serverSocket);
+
+void setCommands();
 
 int main() {
-    std::cout << "Choose port (default " << port << "): ";
-    std::getline(std::cin, input);
-    if (!input.empty()) {
-        try {
-            if (const unsigned short newPort = std::stoi(input); newPort > 0 && newPort <= 65535) port = newPort;
-            else std::cout << "Invalid port range. Using default.\n";
-        } catch (...) {
-            std::cout << "Invalid input. Using default port.\n";
-        }
-    }
+    initialConfiguration();
+    loadBans();
 
     WSADATA wsaData;
     if (const int result = WSAStartup(MAKEWORD(2, 2), &wsaData); result != 0) {
-        std::cout << "WSAStartup failed. Error: " << result << '\n';
+        cout << "WSAStartup failed. Error: " << result << '\n';
+        cin.get();
         return 1;
     }
 
     const auto serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket == INVALID_SOCKET) {
-        std::cout << "Socket creation failed. Error: " << WSAGetLastError() << '\n';
+        cout << "Socket creation failed. Error: " << WSAGetLastError() << '\n';
         WSACleanup();
+        cin.get();
         return 1;
     }
 
-    sockaddr_in serverAddress{};
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddress.sin_port = htons(port);
+    if (const int error = bindAndListen(serverSocket); error != 0) return error;
+    cout << "Server listening on port " << port << "\nType 'help' to see the commands, 'exit' to stop the server.\n\n";
+    acceptClients(serverSocket);
 
-    if (bind(serverSocket, reinterpret_cast<sockaddr *>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
-        std::cout << "Bind failed. Error: " << WSAGetLastError() << '\n';
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
+    cout << "Server shutting down...\n";
+    closesocket(serverSocket);
+    WSACleanup();
+    return 0;
+}
+
+void initialConfiguration() {
+    cout << "Choose port (default " << port << "): ";
+    getline(cin, input);
+    if (!input.empty()) {
+        try {
+            if (const unsigned short newPort = std::stoi(input); newPort > 0 && newPort <= 65535) port = newPort;
+            else cout << "Invalid port range. Using default port.\n";
+        } catch (...) {
+            cout << "Invalid input. Using default port.\n";
+        }
     }
+}
 
-    if (listen(serverSocket, 5) == SOCKET_ERROR) {
-        std::cout << "Listen failed. Error: " << WSAGetLastError() << '\n';
-        closesocket(serverSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    std::cout << "Server listening on port " << port << "\nType 'exit' and press Enter to stop the server...\n";
-
+void acceptClients(const auto &serverSocket) {
     sockaddr_in clientAddress{};
     socklen_t clientAddressLength = sizeof(clientAddress);
 
-    std::thread(listenExit, serverSocket).detach();
+    thread(listenCommands, serverSocket).detach();
 
     while (isRunning) {
         const SOCKET clientSocket = accept(serverSocket, reinterpret_cast<sockaddr *>(&clientAddress),
                                            &clientAddressLength);
         if (clientSocket == INVALID_SOCKET) {
             if (!isRunning) break;
-            std::cout << "Accept failed. Error: " << WSAGetLastError() << '\n';
+            cout << "Accept failed. Error: " << WSAGetLastError() << '\n';
             continue;
-        } {
-            std::lock_guard lock(clientsMutex);
-            clients.push_back(Client{clientSocket,});
         }
 
-        std::thread(handleClient, clientSocket).detach();
+        thread(handleClient, clientSocket).detach();
+    }
+}
+
+int bindAndListen(const auto &serverSocket) {
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(port);
+
+    if (bind(serverSocket, reinterpret_cast<sockaddr *>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+        cout << "Bind failed. Error: " << WSAGetLastError() << '\n';
+        closesocket(serverSocket);
+        WSACleanup();
+        cin.get();
+        return 1;
     }
 
-    std::cout << "Server shutting down...\n";
-    closesocket(serverSocket);
-    WSACleanup();
+    if (listen(serverSocket, 5) == SOCKET_ERROR) {
+        cout << "Listen failed. Error: " << WSAGetLastError() << '\n';
+        closesocket(serverSocket);
+        WSACleanup();
+        cin.get();
+        return 1;
+    }
+
     return 0;
+}
+
+
+void loadBans() {
+    bans.clear();
+    std::ifstream bansInput(BANS_FILE);
+    if (!bansInput) {
+        std::ofstream bansOutput(BANS_FILE);
+        bansOutput.close();
+        cout << "Bans file not found. Created a new file.\n";
+    } else {
+        string line;
+        cout << "Loading banned IPs...\n";
+        while (getline(bansInput, line)) bans.push_back(line);
+        cout << bans.size() << " bans loaded.\n";
+    }
+    bansInput.close();
+}
+
+string clientInfo(const Client &client) {
+    return client.ip + " " + client.username + " (" + std::to_string(client.socket) + ")";
 }
 
 void removeClient(const SOCKET clientSocket) {
     std::erase_if(clients, [clientSocket](const Client &client) { return client.socket == clientSocket; });
 }
 
+string getClientIp(const SOCKET &clientSocket) {
+    sockaddr_in clientAddress;
+    int clientAddressSize = sizeof(clientAddress);
+    if (getpeername(clientSocket, reinterpret_cast<sockaddr *>(&clientAddress), &clientAddressSize) == 0) {
+        char clientIp[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &clientAddress.sin_addr, clientIp, INET_ADDRSTRLEN);
+        return clientIp;
+    }
+
+    return "";
+}
+
 void handleClient(const SOCKET clientSocket) {
+    const string ip = getClientIp(clientSocket);
+    if (ip == "") {
+        cout << "Failed to retrieve client address. Error: " << WSAGetLastError() << '\n';
+        return;
+    }
+
     char buffer[1024];
     int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
     if (bytesReceived <= 0) {
-        std::cout << "Failed to receive username from client.\n";
+        cout << "Failed to receive username from client.\n";
         closesocket(clientSocket);
         return;
     }
+
     buffer[bytesReceived] = '\0';
-    const std::string username(buffer); {
+    const string username(buffer);
+    const Client client = {clientSocket, username, ip};
+
+    if (std::ranges::find(bans, client.ip) != bans.end()) {
+        banClient(client.socket);
+        cout << "Banned IP tried to connect: " << clientInfo(client) << '\n';
+        return;
+    } {
         std::lock_guard lock(clientsMutex);
-        removeClient(clientSocket);
-        clients.push_back({clientSocket, username});
+        removeClient(client.socket);
+        clients.push_back(client);
     }
 
-    std::cout << "Client connected: " << username << " (" << clientSocket << ")\n";
+    cout << "Client connected: " << clientInfo(client) << '\n';
 
     while (true) {
-        bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        bytesReceived = recv(client.socket, buffer, sizeof(buffer) - 1, 0);
         if (bytesReceived <= 0) {
-            std::cout << "Client disconnected: " << username << " (" << clientSocket << ")\n";
+            std::lock_guard lock(clientsMutex);
+            cout << "Client disconnected: " << clientInfo(client) << "\n";
             break;
         }
 
         buffer[bytesReceived] = '\0';
-        std::string message = username + ": " + buffer;
-
-        std::cout << "Received: " << message << '\n';
-        broadcastMessage(message, clientSocket);
+        broadcastMessage(username + ": " + buffer, client.socket);
     } {
         std::lock_guard lock(clientsMutex);
-        removeClient(clientSocket);
+        removeClient(client.socket);
     }
 
-    closesocket(clientSocket);
+    closesocket(client.socket);
 }
 
-void broadcastMessage(const std::string &message, const SOCKET senderSocket) {
+void broadcastMessage(const string &message, const SOCKET senderSocket) {
     std::lock_guard lock(clientsMutex);
-    std::cout << "Socket count: " << clients.size() << '\n';
-    for (const auto &[socket, username]: clients) {
-        if (socket != senderSocket) {
-            if (send(socket, message.c_str(), static_cast<int>(message.size()), 0) == SOCKET_ERROR) {
-                std::cout << "Send failed to client " << username << ". Error: " << WSAGetLastError() << '\n';
+    for (const Client &client: clients) {
+        if (client.socket != senderSocket) {
+            if (send(client.socket, message.c_str(), static_cast<int>(message.size()), 0) == SOCKET_ERROR) {
+                cout << "Send failed to client " << clientInfo(client) << ". Error: " << WSAGetLastError() << '\n';
             }
         }
     }
 }
 
-void listenExit(const SOCKET serverSocket) {
-    std::string input;
+void banClient(const SOCKET &socket) {
+    send(socket, banMessage.c_str(), static_cast<int>(banMessage.size()), 0);
+    closesocket(socket);
+}
+
+void kickClient(const SOCKET &socket) {
+    send(socket, kickMessage.c_str(), static_cast<int>(kickMessage.size()), 0);
+    closesocket(socket);
+}
+
+void listenCommands(const SOCKET serverSocket) {
+    string input;
     while (true) {
-        std::getline(std::cin, input);
+        getline(cin, input);
         if (input == "exit") {
             isRunning = false;
             break;
         }
+
+        if (input == "list") {
+        }
+
+        if (input == "bans") {
+            std::lock_guard lock(clientsMutex);
+            for (const string &ip: bans) cout << ip << '\n';
+            cout << "- end (" << bans.size() << ") -\n\n";
+            continue;
+        }
+
+        if (input == "refresh bans") {
+            std::lock_guard lock(clientsMutex);
+            loadBans();
+            for (const Client &client: clients) {
+                if (std::ranges::find(bans, client.ip) != bans.end()) banClient(client.socket);
+            }
+            cout << '\n';
+            continue;
+        }
+
+        if (input == "clear bans") {
+            std::lock_guard lock(clientsMutex);
+            bans.clear();
+
+            cout << "Removing all bans...\n";
+            std::ofstream bansOutput(BANS_FILE, std::ios::trunc);
+            bansOutput.close();
+            cout << "Bans removed.\n\n";
+        }
+
+        if (input.starts_with("ban ") && input.size() > 4) {
+            std::lock_guard lock(clientsMutex);
+            string ipToBan = input.substr(4);
+            bans.push_back(ipToBan);
+
+            std::ofstream bansOutput(BANS_FILE, std::ios::app);
+            bansOutput << ipToBan << '\n';
+            bansOutput.close();
+
+            for (const Client &client: clients) {
+                if (ipToBan == client.ip) banClient(client.socket);
+            }
+
+            cout << "IP " << ipToBan << " has been banned.\n\n";
+            continue;
+        }
+
+        if (input.starts_with("unban ") && input.size() > 6) {
+            std::lock_guard lock(clientsMutex);
+            string ipToUnban = input.substr(6);
+
+            auto it = std::remove(bans.begin(), bans.end(), ipToUnban);
+            if (it == bans.end()) {
+                cout << "IP " << ipToUnban << " is not banned.\n\n";
+                continue;
+            }
+
+            bans.erase(it, bans.end());
+
+            std::ofstream bansOutput(BANS_FILE, std::ios::trunc);
+            for (const string &ip: bans) bansOutput << ip << '\n';
+            bansOutput.close();
+
+            cout << "IP " << ipToUnban << " has been unbanned.\n\n";
+            continue;
+        }
+
+        if (input.starts_with("kick ") && input.size() > 5) {
+            std::lock_guard lock(clientsMutex);
+            string ipToKick = input.substr(5);
+            bool kicked = false;
+
+            for (const Client &client: clients) {
+                if (ipToKick == client.ip) {
+                    kicked = true;
+                    kickClient(client.socket);
+                }
+            }
+
+            if (kicked) cout << "IP " << ipToKick << " has been kicked.\n\n";
+            else cout << "No client connected by the IP " << ipToKick << ".\n\n";
+
+            continue;
+        }
+
+        if (input == "help") {
+            std::lock_guard lock(clientsMutex);
+            cout << "There'll be commands here.\n\n";
+            continue;
+        }
     }
 
     std::lock_guard lock(clientsMutex);
-    for (const auto &[socket, _]: clients) {
-        closesocket(socket);
+    for (const Client &client: clients) {
+        closesocket(client.socket);
     }
 
     closesocket(serverSocket);
